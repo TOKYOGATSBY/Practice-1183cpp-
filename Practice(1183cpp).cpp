2dd1164b274d1782931d1984a64f1e954eec880e
Grigorii Kolbeev <gold_grigorii@mail.ru>
2025-03-25 10:15:24 +0300

    version 1.1

--- Practice(1183cpp).cpp
+++ Practice(1183cpp).cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 bool matches(char a, char b) {
     return (a == '(' && b == ')') || (a == '[' && b == ']');
-} //проверка символов на образование пары скобок
+}
 
 int main() {
     string s;
@@ -13,15 +13,12 @@ int main() {
 
     int n = (int)s.size();
     if (n == 0) {
-        cout << "\n";
+        cout << endl;
         return 0;
     }
 
-    // dp[i][j] - кратчайшая правильная скобочная последовательность,
-    // в которой s[i..j] входит как подпоследовательность
     static string dp[205][205];
 
-    // Инициализация для подотрезков длины 1
     for (int i = 0; i < n; i++) {
         if (s[i] == '(' || s[i] == ')') {
             dp[i][i] = "()";
@@ -31,15 +28,12 @@ int main() {
         }
     }
 
-    // Перебираем длины подотрезков от 2 до n
     for (int length = 2; length <= n; length++) {
         for (int i = 0; i + length - 1 < n; i++) {
             int j = i + length - 1;
 
-            // Начнём с "большой" строки, чтобы потом минимизировать
             string best(2 * n, 'z');
 
-            // 1) Пробуем разделить подотрезок на два
             for (int k = i; k < j; k++) {
                 string candidate = dp[i][k] + dp[k + 1][j];
                 if (candidate.size() < best.size() ||
@@ -48,9 +42,7 @@ int main() {
                 }
             }
 
-            // 2) Если s[i] и s[j] можно "сопоставить"
             if (matches(s[i], s[j])) {
-                // Внутренняя часть (i+1..j-1)
                 string inside;
                 if (i + 1 <= j - 1) {
                     inside = dp[i + 1][j - 1];
@@ -66,7 +58,7 @@ int main() {
         }
     }
 
-    // Выводим результат для всего отрезка [0..n-1]
-    cout << dp[0][n - 1] << "\n";
+    cout << dp[0][n - 1] << endl;
+
     return 0;
 }
